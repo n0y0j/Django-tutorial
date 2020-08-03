6db840d5a7a6bde7ea6b817bf8ef07b1807fe3c2
n0y0j <n0y0j77@gmail.com>
2020-08-03 16:49:14 +0900

    Implement login, comment.

--- mysite/urls.py
+++ mysite/urls.py
@@ -14,9 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from django.conf.urls import include, url
+from django.contrib.auth import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/login', views.login, name='login'),
+    path('accounts/logout', views.logout, name='logout', kwargs={'next_page': '/'}),
     path('', include('blog.urls')),
 ]
