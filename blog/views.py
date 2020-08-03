acb753d3a082bdda1b126e4aa159bb98abc82e01
n0y0j <n0y0j77@gmail.com>
2020-08-03 16:01:16 +0900

    Implement preview, publishing, remove.

--- blog/views.py
+++ blog/views.py
@@ -20,7 +20,6 @@ def post_new(request):
       # why? Authot 필드가 없지만 추가를 해야하기 때문
       post = form.save(commit=False)
       post.author = request.user
-      post.published_date = timezone.now()
       post.save()
       # 새 글을 작성후 post_detail로 바로 이동
       return redirect('post_detail', pk=post.pk)
@@ -35,9 +34,23 @@ def post_edit(request, pk):
     if form.is_valid():
       post = form.save(commit=False)
       post.author = request.user
-      post.published_date = timezone.now()
       post.save()
       return redirect('post_detail', pk=post.pk)
   else:
     form = PostForm(instance=post)
   return render(request, 'blog/post_edit.html', {'form' : form})
+
+def post_draft_list(request):
+  # 코드로 발행되지 않은 글 목록을 가져옴, 오름차순 정렬
+  posts = Post.objects.filter(published_date__isnull=True).order_by('created_date')
+  return render(request, 'blog/post_draft_list.html', {'posts':posts})
+
+def post_publish(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.publish()
+    return redirect('post_detail', pk=pk)
+
+def post_remove(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.delete()
+    return redirect('post_list')
